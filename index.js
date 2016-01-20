51d0d04450d917bbf49c590c9223a6cf40fa1c37
Shane Murtagh <murtaghshane3@gmail.com>
2016-01-20 16:36:23 +0000

    Second Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var config = require('config');
+var path = require('path');
+var cors = require('cors'); //package to allow CORS requests
+
+var app = express();
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/public')); // set the public folder to serve public assets
+
+// set up our one route to the index.html file
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/index.html'))
+});
+
+// start the local server on port 8080 (http://localhost:8080)
+app.listen(config.port);
+console.log('Election App launched.');
